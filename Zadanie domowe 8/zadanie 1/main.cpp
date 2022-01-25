d154f9ed858921569b1a0426d3e041db28747182
KoTubA <szymon.kotarba988@gmail.com>
2022-01-25 15:24:52 +0100

    Add the solution of the eighth exercise

---
+++ Zadanie domowe 8/zadanie 1/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+class Fraction {
+public:
+    int a;
+    int b;
+    double dvalue;
+
+    Fraction(int ap, int bp): a(ap), b(bp) {
+        dvalue = (double)ap/bp;
+    };
+
+    operator double()
+    {
+        return this->dvalue;
+    }
+
+    Fraction&operator=(Fraction &drugi) {
+        if (this == &drugi)
+            return *this;
+
+        this->a = drugi.a;
+        this->b = drugi.b;
+        this->dvalue = drugi.dvalue;
+        return *this;
+    }
+
+    friend std::ostream & operator<<(std::ostream &, Fraction &);
+    friend std::istream & operator>>(std::istream &, Fraction &);
+};
+
+std::ostream & operator<<(std::ostream & wyjscie, Fraction  &x) {
+    wyjscie << "Zawartosc obiektu:\na: " << x.a << endl << "b: " << x.b << endl << "dvalue: " << x.dvalue;
+    return wyjscie;
+}
+
+std::istream & operator>>(std::istream & wejscie, Fraction  &x) {
+    std::cout << "Licznik: ";
+    wejscie >> x.a;
+    std::cout << "Mianownik: ";
+    wejscie >> x.b;
+    x.dvalue = x.a / x.b;
+    return wejscie;
+}
+
+int main()
+{
+    Fraction x1(1,3);
+    Fraction x2(1,2);
+
+    x1 = x2;
+    cin >> x2;
+    cout << x2 << endl;
+
+    std::cout << "Rzutowanie ulamka na dobule:"  << double(x1);
+
+    return 0;
+}
