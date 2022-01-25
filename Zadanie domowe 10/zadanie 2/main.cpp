c54ff3dbf260d41229ee64a34be90181f09f41af
KoTubA <szymon.kotarba988@gmail.com>
2022-01-25 21:32:56 +0100

    Add the solution of the tenth exercises

--- Zadanie domowe 10/zadanie 2/main.cpp
+++ Zadanie domowe 10/zadanie 2/main.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
+#include <map>
 
 int main() {
     std::ifstream file("../text.txt");
     char ch;
-    std::vector<int> count = {0};
+    std::map<char, int> letters;
     if(file.fail()) {
         std::cout << "Nie mozna bylo otworzyc pliku!";
     }
     else {
         while(file.get(ch)){
-            if (isalpha(ch)) count[0]++;
+            if (isalpha(ch)) {
+                auto search = letters.find(ch);
+                if(search != letters.end()) {
+                    letters[ch]++;
+                }
+                else {
+                    letters.insert(std::pair<char,int>(ch, 1));
+                }
+            }
         }
-        std::cout << count[0] << " characters\n";
+        for(const auto& elem : letters) {
+            std::cout << elem.first << " " << elem.second << std::endl;
+        }
     }
 
     return 0;
